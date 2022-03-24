bcf9f1b1472cd2d60152e9e470610835d5be7f7c
Jonas <jonastibisch@gmail.com>
2022-03-24 21:11:10 +0100

    added comments and meaningful variables

--- simon_says.py
+++ simon_says.py
@@ -3,58 +3,58 @@
 from microbit import *
 import random
 
-
-    
-    
-
-
-
-
-
- upleft= Image("99000:99000:00000:00000:00000")
+#CREATION OF THE FOUR IMAGES
+upleft= Image("99000:99000:00000:00000:00000")
 upright = Image("00099:00099:00000:00000:00000")
 downleft = Image("00000:00000:00000:99000:99000")
 downright = Image("00000:00000:00000:00099:00099")
-    
 
-User_input = []
-Reihenfolge = []
-Bilder = [upleft,upright,downleft,downright]
+sequence = []
+pictures = [upleft,upright,downleft,downright]
 
 while True:
+    #DEFINE LIST IN WHILE-LOOP, SO USER MUST PRESS WHOLE SEQUENCES AGAIN
+    user_input = []
+    
     #CREATION OF SEQUENCE
     s = random.randint(0,3)
-    Reihenfolge.append(Bilder[s])
-    for i in range(len(Reihenfolge)):
-        display.show(Reihenfolge[i])
+    sequence.append(pictures[s])
+
+    #SHOW SEQUENCE
+    for i in range(len(sequence)):
+        display.show(sequence[i])
         sleep(700)
         display.clear()
         sleep(500)
+
     ###MOVEMENT WITH ACCELEROMETER 
     # USER INPUTS GET INSERTED IN TO LIST
-    while len(User_input) < len(Reihenfolge):
+    while len(user_input) < len(sequence):
         LR = accelerometer.get_x()
         WD = accelerometer.get_y()
         if LR < -110 and WD < 110:
             display.show(upleft)
             if button_b.get_presses():
-                User_input.append(upleft)
+                user_input.append(pictures[0])
+       
         elif LR < 110 and WD < 110:
             display.show(upright)
             if button_b.get_presses():
-                User_input.append(upright)
+                user_input.append(pictures[1])
+        
         elif LR < -110 and WD > 110:
             display.show(downleft)
             if button_b.get_presses():
-                User_input.append(downleft)         
+                user_input.append(pictures[2])         
+        
         elif LR > 110 and WD > 110:
             display.show(downright)
             if button_b.get_presses():
-                User_input.append(downright)
+                user_input.append(pictures[3])
+
 #CHEK IF USER PRESSED THE RIGHT SEQUENCE           
-    if User_input != Reihenfolge:
+    if user_input != sequence:
         break
     display.clear()
     sleep(2000)
 display.scroll("Game Over")
-
